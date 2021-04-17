5c88c4812358e59281568d40ffa4d7031842c33d
Emir Çetin <emircetin98@gmail.com>
2021-04-18 00:39:05 +0300

    register page sayfası yazıldı

--- lib/ui/auth/register/register_page.dart
+++ lib/ui/auth/register/register_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sahiplen/core/components/button/special_button.dart';
+import 'package:sahiplen/core/components/button/special_text_button.dart';
+import 'package:sahiplen/core/components/textformfield/auth_text_form_field.dart';
+import 'package:sahiplen/core/constants/router_constants.dart';
 import 'package:sahiplen/core/extensions/app_extensions.dart';
 import 'package:sahiplen/core/base/view/base_view.dart';
 import 'package:sahiplen/ui/auth/register/register_view_model.dart';
@@ -7,15 +11,93 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseView<RegisterViewModel>(
-        viewModel: RegisterViewModel(),
-        onModelReady: (model) {
-          model.setContext(context);
-          model.init();
-        },
-        onPageBuilder: (BuildContext context, RegisterViewModel viewModel) {
-          return Scaffold(
-            body: Container(),
-          );
-        });
+      viewModel: RegisterViewModel(),
+      onModelReady: (model) {
+        model.setContext(context);
+        model.init();
+      },
+      onPageBuilder: (BuildContext context, RegisterViewModel viewModel) {
+        return Scaffold(
+          backgroundColor: context.theme.primaryColor,
+          body: body(context, viewModel),
+        );
+      },
+    );
+  }
+
+  SafeArea body(BuildContext context, RegisterViewModel viewModel) {
+    return SafeArea(
+      child: Center(
+        child: Container(
+          height: context.height * .9,
+          decoration: BoxDecoration(borderRadius: context.borderRadiusMedium, color: Colors.white),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: context.mediumValue),
+                helloTextField(context),
+                fromField(viewModel, context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding helloTextField(BuildContext context) {
+    return Padding(
+      padding: context.paddingMediumHighTop,
+      child: Text(
+        'KAYIT OL',
+        style: context.textTheme.headline4,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Form fromField(RegisterViewModel viewModel, BuildContext context) {
+    return Form(
+      key: viewModel.formKey,
+      child: Column(
+        children: [
+          SizedBox(height: context.mediumValue),
+          AuthTextFormField(
+            labelText: 'Adın Soyadın',
+            saveAttempted: viewModel.saveAttempted,
+            keyboardType: TextInputType.text,
+            onSaved: (_displayName) => viewModel.displayName = _displayName,
+            validator: (_displayName) => viewModel.appRepository.displayNameCheck(_displayName),
+          ),
+          AuthTextFormField(
+            labelText: 'E-Posta',
+            saveAttempted: viewModel.saveAttempted,
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (_email) => viewModel.email = _email,
+            validator: (_email) => viewModel.appRepository.emailCheck(_email),
+          ),
+          AuthTextFormField(
+            labelText: 'Parola',
+            saveAttempted: viewModel.saveAttempted,
+            keyboardType: TextInputType.visiblePassword,
+            textInputAction: TextInputAction.done,
+            obscureText: viewModel.obscure,
+            onSaved: (_password) => viewModel.password = _password,
+            validator: (_password) => viewModel.appRepository.passwordCheck(_password),
+            suffixIcon: IconButton(
+              icon: Icon(Icons.remove_red_eye_rounded, color: viewModel.obscure ? Colors.grey : context.theme.primaryColor),
+              onPressed: () => viewModel.obscureUpdate(),
+            ),
+          ),
+          SpecialButton(
+            text: 'Kayıt Ol',
+            isLoading: viewModel.isLoading,
+            onPressed: () async => await viewModel.registerWithMail(),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/ui/auth/register/register_view_model.dart
+++ lib/ui/auth/register/register_view_model.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:sahiplen/common/model/app_user.dart';
 import 'package:sahiplen/core/base/modelview/base_view_model.dart';
+import 'package:sahiplen/core/constants/error_constans.dart';
 
 class RegisterViewModel extends BaseViewModel {
+  String? email, password, displayName;
+  late bool obscure;
+  late GlobalKey<FormState> formKey;
+  late bool saveAttempted;
   @override
-  void init() {}
+  void init() {
+    formKey = GlobalKey<FormState>();
+    saveAttempted = false;
+    obscure = false;
+  }
+
   @override
   void setContext(BuildContext context) {
     this.context = context;
+  }
+
+  void obscureUpdate() {
+    obscure = !obscure;
+    setState();
+  }
+
+  Future<bool> registerWithMail() async {
+    try {
+      isLoadingSet = true;
+
+      if (formKey.currentState!.validate()) {
+        formKey.currentState!.save();
+        try {
+          var tempAppUser = AppUser(displayName: displayName!.trim(), email: email!.trim());
+          return await appRepository.registerWithMail(tempAppUser, password!);
+        } catch (e) {
+          showSnackBar(AuthErrorConstants.instance.getAuthErrorText(e.toString()));
+          return false;
+        }
+      } else {
+        saveAttempted = true;
+        return false;
+      }
+    } finally {
+      isLoadingSet = false;
+    }
   }
 }
